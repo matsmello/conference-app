0a064f7100735a57c308159464d3890538eeaa5b
Matheus Mello <matheusmello_22@hotmail.com>
2020-08-04 14:55:57 -0300

    Receive message/feedback from server and added to queue using amqplib/rabbitmq

--- server/services/Feedback.js
+++ server/services/Feedback.js
@@ -1,9 +1,9 @@
 /* eslint-disable class-methods-use-this */
-const url = require('url');
-const axios = require('axios');
-const crypto = require('crypto');
-
-const CircuitBreaker = require('../lib/CircuitBreaker');
+const url = require("url");
+const axios = require("axios");
+const crypto = require("crypto");
+const amqplib = require("amqplib");
+const CircuitBreaker = require("../lib/CircuitBreaker");
 
 const circuitBreaker = new CircuitBreaker();
 
@@ -14,17 +14,32 @@ class FeedbackService {
     this.cache = {};
   }
 
+  async addEntry(name, title, message) {
+    const q = "feedback";
+    const conn = await amqplib.connect("amqp://localhost");
+
+    const ch = await conn.createChannel();
+
+    await ch.assertQueue(q);
+
+    const qm = JSON.stringify({ name, title, message });
+    return ch.sendToQueue(q, Buffer.from(qm, "utf-8"));
+  }
+
   async getList() {
-    const { ip, port } = await this.getService('feedback-service');
+    const { ip, port } = await this.getService("feedback-service");
     return this.callService({
-      method: 'get',
+      method: "get",
       url: `http://${ip}:${port}/list`,
     });
   }
 
   async callService(requestOptions) {
     const parsedUrl = url.parse(requestOptions.url);
-    const cacheKey = crypto.createHash('md5').update(requestOptions.method + parsedUrl.path).digest('hex');
+    const cacheKey = crypto
+      .createHash("md5")
+      .update(requestOptions.method + parsedUrl.path)
+      .digest("hex");
 
     const result = await circuitBreaker.callService(requestOptions);
 
@@ -42,7 +57,9 @@ class FeedbackService {
   }
 
   async getService(servicename) {
-    const response = await axios.get(`${this.serviceRegistryUrl}/find/${servicename}/${this.serviceVersionIdentifier}`);
+    const response = await axios.get(
+      `${this.serviceRegistryUrl}/find/${servicename}/${this.serviceVersionIdentifier}`
+    );
     return response.data;
   }
 }
